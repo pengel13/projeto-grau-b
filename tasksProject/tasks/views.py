9f5ffdae10180111ad5d7554ddb72e0c176e36a1
Pedro Engel <pedrototiengel@gmail.com>
2025-06-03 20:48:52 -0300

    init: início do projeto

---
+++ tasksProject/tasks/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import  User
+from rest_framework import permissions, viewsets
+
+from tasksProject.tasks.serializers import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
