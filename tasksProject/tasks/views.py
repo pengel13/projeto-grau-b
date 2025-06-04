36a038ca58147425118056979c0ee9547aea4fdf
Pedro Engel <pedrototiengel@gmail.com>
2025-06-03 21:52:48 -0300

    feat: adicionado logs e diagrama ER

--- tasksProject/tasks/views.py
+++ tasksProject/tasks/views.py
@@ -9,6 +9,8 @@ from tasksProject.tasks.serializers import (
 )
 from tasksProject.tasks.models import Task, Task_User
 from rest_framework.response import Response
+import logging
+from django.http.response import Http404
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,6 +22,16 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def retrieve(self, request, pk=None):
+        logger = logging.getLogger("retrieveUsers")
+        try:
+            user = self.get_object()
+            serializer = self.get_serializer(user)
+            return Response(data=serializer.data)
+        except Http404:
+            logger.error(f"Usuário ID {pk} não encontrado")
+            return Response({"detail": f"Usuário {2} não encontrado"})
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     """
@@ -28,6 +40,15 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def create(self, request):
+        logger = logging.getLogger("createTasks")
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            logger.info("Task created succesfully")
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
 class Tasks_UserViewSet(viewsets.ModelViewSet):
@@ -44,10 +65,14 @@ class Tasks_UserViewSet(viewsets.ModelViewSet):
         return TaskUserSerializer
 
     def list(self, request):
+        logger = logging.getLogger("listTaskUsers")
         tasks = Task_User.objects.all()
         userID = self.request.query_params.get("assignedTo")
         if userID != None:
             user = User.objects.filter(id=userID).first()
             tasks = tasks.filter(user=user)
+            if len(tasks) == 0:
+                logger.error(f"There are no users with the id {userID}")
+
         serializer = self.get_serializer(tasks, many=True)
         return Response(serializer.data)
